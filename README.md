e9aa08ab0ba8ae5da16b8d91e82d4c5076e35040
KENNETH PHANG <bunnyppl@hotmail.com>
2022-07-14 13:53:22 +0800

    new

---
+++ README.md
@@ -0,0 +1,35 @@
+## Workshop 13
+
+* Run the spring boot app
+
+Linux/Macos
+```
+mvn spring-boot:run -Dspring-boot:run.arguments=--dataDir=/opt/tmp/data
+```
+
+Windows 10/11
+```
+mvn spring-boot:run -Dspring-boot:run.arguments=--dataDir=C:/data
+```
+
+* Incorporate test starter dependencies to the pom.xml
+
+```
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-test</artifactId>
+    <version>2.7.1</version>
+    <scope>test</scope>
+</dependency>
+
+```
+
+* In order to execute test cases
+
+```
+mvn test
+```
+
+```
+mvn package
+```
---
+++ src/main/java/vttp2022b2/webapp/workshop13/model/Contact.java
@@ -0,0 +1,71 @@
+package vttp2022b2.webapp.workshop13.model;
+
+import java.io.Serializable;
+import java.util.Random;
+
+public class Contact implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private String email;
+    private int phoneNumber;
+    private String id;
+
+    public Contact() {
+        this.id = this.generateId(8);
+    }
+
+    public Contact(String name, String email, int phoneNumber) {
+        this.id = this.generateId(8);
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Contact(String id, String name, String email, int phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    private synchronized String generateId(int numChars) {
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < numChars) {
+            sb.append(Integer.toHexString(r.nextInt()));
+        }
+        return sb.toString().substring(0, numChars);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
